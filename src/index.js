5220e0dfb1c943f11294b06e196da015900081a5
Rafael Moura <rafaelnmoura20@gmail.com>
2020-07-26 02:45:54 -0300

    Add nodemon

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
 
@@ -6,5 +6,9 @@ app.get('/', ( request, response) => {
     return response.json({ message: 'Hello World' });
 });
 
-app.listen(3333)
+app.listen(3333, () => {
+    console.log("🚀 Backend Started! 😅");
+})
+
+
 
